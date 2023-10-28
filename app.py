7ab2b7a57bb9081dfc797b0209a45b12a3374edb
r4stl1n <royal.r4stl1n@gmail.com>
2023-10-28 16:38:36 -0700

    Added vuln code

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import subprocess
+import sys
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    user_input = "foo && cat /etc/passwd" # value supplied by user
+    subprocess.call("grep -R {} .".format(user_input), shell=True)
     return 'Just a quick hello-world'
 
